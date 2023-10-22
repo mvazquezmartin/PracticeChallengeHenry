398710572d0da8c212f9f68a245d644ebef9ddda
mvazquez <matias.vazquezmartin@gmail.com>
2023-10-22 12:50:17 -0300

    feat: 01

--- 01.js
+++ 01.js
@@ -7,14 +7,19 @@
 function soloNumeros(array) {
   // La funcion llamada 'soloNumeros' recibe como argumento un arreglo de enteros y strings llamado 'array'.
   // Debe devolver un arreglo con solo los enteros.
-  // Ej: 
+  // Ej:
   // soloNumeros([1, 'Henry', 2]) debe retornar [1, 2]
 
   // Tu código aca:
-
+  // 1
+  // return array.filter(e => typeof e === 'number' && !isNaN(e));
+  const onlyNumber = [];
+  array.forEach((e) => {
+    if (Number.isInteger(e)) onlyNumber.push(e);
+  });
+  return onlyNumber;
 }
 
 // No modifiques nada debajo de esta linea //
 
-
-module.exports = soloNumeros
+module.exports = soloNumeros;
